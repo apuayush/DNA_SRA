06523e4f6d60ef0a64c22954858c754127759ed5
Surya <surya.thiru001@gmail.com>
2018-09-09 04:56:39 +0530

    ignoring angle calc for now

--- utils.py
+++ utils.py
@@ -1,16 +1,16 @@
 from skimage.filters import threshold_minimum
 import numpy as np
 import cv2
+
+
 def threshold(image):
     """ Does thresholding
     """
-    from skimage.filters import threshold_minimum
-    import numpy as np    
-
     image = np.asarray(img)
     image = np.dot(image[...,:3], [0.299, 0.587, 0.114])
     thresh = threshold_minimum(image[:,:])
     return image[:,:] > thresh
+
 
 def denoise(image):
     """ Bilateral filtering with opencv on image segments
