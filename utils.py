f27cd072bd72493e3c80c73bf93cf7e5387c51ea
Surya <surya.thiru001@gmail.com>
2018-09-09 00:05:41 +0530

    update dir and add denoise func

--- utils.py
+++ utils.py
@@ -1,7 +1,16 @@
-def segment(image):
-    from skimage.filters import threshold_minimum
-    import numpy as np
+from skimage.filters import threshold_minimum
+import numpy as np
+import cv2
 
+
+def segment(image):
     image = np.dot(image[...,:3], [0.299, 0.587, 0.114])
     thresh = threshold_minimum(np.asarray(image)[:,:,0])
     return np.asarray(image)[:,:,0] > thresh
+
+
+def denoise(image):
+    """ Bilateral filtering with opencv on image segments
+    """
+    new = cv2.bilateralFilter(image, 9, 75, 75)
+    return new
